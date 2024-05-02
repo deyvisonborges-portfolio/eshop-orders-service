771ee3170bb633be46fb5fc3e09869579c58f9b0
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-05-02 08:17:48 -0300

    replace orde rabbitmq queue constants

--- src/main/java/com/deyvisonborges/service/orders/core/modules/management/order/event/OrderEventConstants.java
+++ src/main/java/com/deyvisonborges/service/orders/core/modules/management/order/event/OrderEventConstants.java
@@ -2,7 +2,7 @@ package com.deyvisonborges.service.orders.core.modules.management.order.event;
 
 public final class OrderEventConstants {
   public static final String ORDER_EXCHANGE_NAME = "order.v1.direct";
-  public static final String ORDER_QUEUE_NAME = "order.v1.direct";
+  public static final String ORDER_QUEUE_NAME = "order.v1.queue";
   public static final String ORDER_HISTORY_QUEUE_NAME = "order.v1.queue.history";
   public static final String ORDER_CREATED_EVENT_ROUTING_KEY = "order.v1.event.order-created";
 }
