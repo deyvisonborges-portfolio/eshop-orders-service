452fb7e6f5c957b9fa7104030ecceeec484daec2
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-05-10 14:32:16 -0300

    resolve rabbitmq config

--- src/main/java/com/deyvisonborges/service/orders/OrdersApplication.java
+++ src/main/java/com/deyvisonborges/service/orders/OrdersApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.AbstractEnvironment;
 
-@SpringBootApplication
 @EnableRabbit
+@SpringBootApplication
 public class OrdersApplication {
   public static void main(String[] args) {
     System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "dev");
