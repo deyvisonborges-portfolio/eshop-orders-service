5175e34cce68582d5c6dc8859c5bd7829d67c197
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-07-08 10:36:36 -0300

    resolve jackson instant convertion

---
+++ src/main/java/com/deyvisonborges/service/orders/app/config/CacheConfig.java
@@ -0,0 +1,34 @@
+package com.deyvisonborges.service.orders.app.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.util.Objects;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+  private final RedisTemplate<String, Object> redisTemplate;
+
+  public CacheConfig(RedisTemplate<String, Object> redisTemplate) {
+    this.redisTemplate = redisTemplate;
+  }
+
+  @Bean
+  public RedisCacheManager cacheManager() {
+    return RedisCacheManager.builder(Objects.requireNonNull(redisTemplate.getConnectionFactory()))
+      .cacheDefaults(redisCacheConfiguration())
+      .build();
+  }
+
+  private RedisCacheConfiguration redisCacheConfiguration() {
+    return RedisCacheConfiguration.defaultCacheConfig()
+      .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getValueSerializer()));
+  }
+}
---
+++ src/main/java/com/deyvisonborges/service/orders/app/config/RedisConfig.java
@@ -0,0 +1,53 @@
+package com.deyvisonborges.service.orders.app.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.serializer.support.SerializationFailedException;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    RedisTemplate<String, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(redisConnectionFactory);
+
+    Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+    objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
+    objectMapper.registerModule(new JavaTimeModule());
+    template.setKeySerializer(new StringRedisSerializer());
+    template.setValueSerializer(serializer);
+    template.setHashKeySerializer(new StringRedisSerializer());
+    template.setHashValueSerializer(serializer);
+
+    return template;
+  }
+  
+  @Bean
+  public RedisCacheConfiguration cacheConfiguration(ObjectMapper mapper) {
+    return RedisCacheConfiguration.defaultCacheConfig()
+      .entryTtl(Duration.ofMinutes(5))
+      .disableCachingNullValues()
+      .serializeValuesWith(
+        RedisSerializationContext.SerializationPair.fromSerializer(
+          new GenericJackson2JsonRedisSerializer(mapper)
+        )
+      );
+  }
+}
