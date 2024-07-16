5109f58f7cfdce1685667cf27370da8026dc57a4
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-07-15 21:34:45 -0300

    configurate public and private key

--- src/main/java/com/deyvisonborges/service/orders/app/config/SpringSecurityConfig.java
+++ src/main/java/com/deyvisonborges/service/orders/app/config/SpringSecurityConfig.java
@@ -4,12 +4,23 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,18 +31,27 @@ public class SpringSecurityConfig {
   @Value("${pwt.private.key}")
   private RSAPrivateKey privateKey;
 
-  @SuppressWarnings("unused")
-  private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
     .authorizeHttpRequests(
-      // requer autenticacao para qualquer requisicao
       authorize -> authorize.anyRequest().authenticated())
-      // disable localmente temporariamente
       .csrf(csrf -> csrf.disable())
-      // usarei o jwt no projeto e ativei as configuracoes padroes
       .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-      // define que nao preciso guardar nada em sessao
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
       return http.build();
+  }
+
+
+  @Bean
+  JwtDecoder jwtDncoder() {
+    return NimbusJwtDecoder.withPublicKey(publicKey).build();
+  }
+
+  @Bean
+  JwtEncoder jwtEncoder() {
+    JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
+    final var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+    return new NimbusJwtEncoder(jwks);
   }
 }
