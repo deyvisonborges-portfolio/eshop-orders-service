054f2f5412d2a1e62173b8d761f646972993d3b4
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-07-28 20:40:52 -0300

    configure oauth resource server

--- src/main/java/com/deyvisonborges/service/orders/OrdersApplication.java
+++ src/main/java/com/deyvisonborges/service/orders/OrdersApplication.java
@@ -3,12 +3,13 @@ package com.deyvisonborges.service.orders;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.core.env.AbstractEnvironment;
 
 @EnableRabbit
 @EnableCaching
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class OrdersApplication {
   public static void main(String[] args) {
     System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "test");
